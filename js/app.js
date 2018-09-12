faafd7211f1406db07cbd4b22982b8ad2e799e00
Dmitry Sharabin <dmitrysharabin@gmail.com>
2018-09-12 23:37:22 +0300

    feat: Add game initialization
    Add cards to the game board. Each card has a symbol on it
    that is randomly selected from the array of theme symbols.
    Every symbol appears on cards twice.

---
+++ js/app.js
@@ -0,0 +1,41 @@
+// Basic properties of the game
+
+// Number of pairs of cards
+const NUMBER_OF_PAIRS = 8;
+
+// Array of symbols that are on one side of the cards - theme of the game
+const theme = ['🐶', '🐱', '🐭', '🐹', '🐻', '🐼', '🐵', '🐯'];
+
+// Game Initialization
+const init = function () {
+  // There are two cards with the same symbol on it.
+  // Use every symbol of the theme twice
+  const symbols = [];
+  symbols.push(...theme, ...theme);
+
+  // Add cards to the game board: every card is an element of
+  // the unordered list with id game-board
+  const gameBoard = document.querySelector('#game-board');
+
+  // Use DocumentFragment to efficiently add cards to the game board
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < 2 * NUMBER_OF_PAIRS; i++) {
+
+    // Create card
+    const card = document.createElement('li');
+    card.classList.add('card');
+
+    // Symbol on a card is a random element of the array symbols.
+    // To simulate shuffle of the cards pick one random element
+    // of the array symbols and remove it from there.
+    // Math.floor(Math.random() * symbols.length) is index of the element
+    card.textContent = symbols.splice(Math.floor(Math.random() * symbols.length), 1);
+
+    fragment.appendChild(card);
+  }
+  // Add cards to the game board
+  gameBoard.appendChild(fragment);
+}
+
+init();
