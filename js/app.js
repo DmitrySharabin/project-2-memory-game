884d4c21f82ce317af6fe18c0c7ccd37515f0255
Dmitry Sharabin <dmitrysharabin@gmail.com>
2018-09-14 09:43:33 +0300

    refactor: Move main logic out of the scope of Init function

--- js/app.js
+++ js/app.js
@@ -12,87 +12,82 @@ const randomInt = function (num) {
   return Math.floor(Math.random() * num);
 }
 
-// Game Initialization
-const init = function () {
-  // There are two cards with the same symbol on it.
-  // Use every symbol of the theme twice
-  const symbols = [...theme, ...theme];
+// There are two cards with the same symbol on it.
+// Use every symbol of the theme twice
+const symbols = [...theme, ...theme];
 
-  // Add cards to the game board: every card is an element of
-  // the unordered list with id game-board
-  const gameBoard = document.querySelector('#game-board');
+// Add cards to the game board: every card is an element of
+// the unordered list with id game-board
+const gameBoard = document.querySelector('#game-board');
 
-  // Use DocumentFragment to efficiently add cards to the game board
-  const fragment = document.createDocumentFragment();
+// Use DocumentFragment to efficiently add cards to the game board
+const fragment = document.createDocumentFragment();
 
-  for (let i = 0; i < 2 * NUMBER_OF_PAIRS; i++) {
+for (let i = 0; i < 2 * NUMBER_OF_PAIRS; i++) {
 
-    // Create card
-    const card = document.createElement('li');
-    card.classList.add('card');
+  // Create card
+  const card = document.createElement('li');
+  card.classList.add('card');
 
-    // Symbol on a card is a random element of the array symbols.
-    // To simulate shuffle of the cards pick one random element
-    // of the array symbols and remove it from there.
-    // randomInt(symbols.length) is index of the element
-    card.textContent = symbols.splice(randomInt(symbols.length), 1);
+  // Symbol on a card is a random element of the array symbols.
+  // To simulate shuffle of the cards pick one random element
+  // of the array symbols and remove it from there.
+  // randomInt(symbols.length) is index of the element
+  card.textContent = symbols.splice(randomInt(symbols.length), 1);
 
-    fragment.appendChild(card);
-  }
-  // Add cards to the game board
-  gameBoard.appendChild(fragment);
-
-  // Shows if only one card (that is not matched) is flipped
-  let isOnlyOneCardFlipped = false;
-
-  // Number of pairs of cards that are matched
-  let numOfMatchedPairs = 0;
-
-  // Shows if all cards are matched and the gave is over
-  let gameOver = false;
-
-  // Add Event Listener to the game board
-  gameBoard.addEventListener('click', function(event) {
-    // Event fires only on cards that are not flipped or matched:
-    //  only on cards that have only one class 'card'
-    if (!gameOver && event.target.classList.length === 1) {
-      event.target.classList.add('flipped');
-      if (isOnlyOneCardFlipped) {
-        // Select flipped cards
-        const flippedCards = gameBoard.querySelectorAll('.flipped');
-
-        // Check if the flipped cards match
-        if (flippedCards[0].textContent === flippedCards[1].textContent) {
-          // Increase number of matched pairs
-          numOfMatchedPairs++;
-
-          // and mark the cards as matched
-          for (const flippedCard of flippedCards) {
-            flippedCard.classList.add('matched');
-          }
-        }
-
-        // Flip back cards. If they matched they will stay 'flipped'
-        for (const flippedCard of flippedCards) {
-          flippedCard.classList.remove('flipped');
-        }
-
-        // There is no flipped cards that are not mached
-        isOnlyOneCardFlipped = false;
-      } else {
-        // There is only one flipped card that is not matched
-        isOnlyOneCardFlipped = true;
-      }
-
-      // Check if all cards are matched
-      if (numOfMatchedPairs === NUMBER_OF_PAIRS) {
-        // The game is over
-        gameOver = true;
-        // TODO: add modal to show user that the game is over
-        
-      }
-    }
-  });
+  fragment.appendChild(card);
 }
+// Add cards to the game board
+gameBoard.appendChild(fragment);
 
-init();
+// Shows if only one card (that is not matched) is flipped
+let isOnlyOneCardFlipped = false;
+
+// Number of pairs of cards that are matched
+let numOfMatchedPairs = 0;
+
+// Shows if all cards are matched and the gave is over
+let gameOver = false;
+
+// Add Event Listener to the game board
+gameBoard.addEventListener('click', function(event) {
+  // Event fires only on cards that are not flipped or matched:
+  //  only on cards that have only one class 'card'
+  if (!gameOver && event.target.classList.length === 1) {
+    event.target.classList.add('flipped');
+    if (isOnlyOneCardFlipped) {
+      // Select flipped cards
+      const flippedCards = gameBoard.querySelectorAll('.flipped');
+
+      // Check if the flipped cards match
+      if (flippedCards[0].textContent === flippedCards[1].textContent) {
+        // Increase number of matched pairs
+        numOfMatchedPairs++;
+
+        // and mark the cards as matched
+        for (const flippedCard of flippedCards) {
+          flippedCard.classList.add('matched');
+        }
+      }
+
+      // Flip back cards. If they matched they will stay 'flipped'
+      for (const flippedCard of flippedCards) {
+        flippedCard.classList.remove('flipped');
+      }
+
+      // There is no flipped cards that are not mached
+      isOnlyOneCardFlipped = false;
+    } else {
+      // There is only one flipped card that is not matched
+      isOnlyOneCardFlipped = true;
+    }
+
+    // Check if all cards are matched
+    if (numOfMatchedPairs === NUMBER_OF_PAIRS) {
+      // The game is over
+      gameOver = true;
+      // TODO: add modal to show user that the game is over
+
+    }
+  }
+});
